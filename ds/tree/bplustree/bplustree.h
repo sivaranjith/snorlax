293880bdb8e0410899fc869d8dc7b4e77f2bbdf4
Sivaranjith Kumar <sivaranjithkumarten@gmail.com>
2016-10-22 01:19:12 +0530

    insertion problem when internal node splits

--- ds/tree/bplustree/bplustree.h
+++ ds/tree/bplustree/bplustree.h
@@ -22,8 +22,6 @@ struct bPlusTreeNode* createBPlusTreeNode(int bTreeNodeSize)
 	temp->nodeSize = bTreeNodeSize;
 	temp->leafHead = temp->dataRoot = (struct bPlusTreeDataNode*)malloc(sizeof(struct bPlusTreeDataNode));
 	temp->dataRoot->val = (int*)malloc(sizeof(int)*bTreeNodeSize);
-	temp->dataRoot->size = 0;
-	temp->dataRoot->parent = NULL;
 	temp->dataRoot->child = (struct bPlusTreeDataNode**)malloc(sizeof(struct bPlusTreeDataNode*)*(bTreeNodeSize+1));
 	for(;i<=bTreeNodeSize;++i)
 	{
@@ -39,14 +37,19 @@ struct bPlusTreeNode* createBPlusTreeNode(int bTreeNodeSize)
 int binarySearchNode(struct bPlusTreeDataNode *dataRoot,int secVal)
 {
 	int start,end,mid,val;
-	
+
 	for(start=0, end=dataRoot->size; start != end; )
 	{
 		mid = (start + end)/2;
 		val = *(dataRoot->val + mid);
+
 		if(val == secVal)
 		{
-			return dataRoot;
+			return mid;
+		}
+		else if(start + 1 == end)
+		{
+			break;
 		}
 		else if(val < secVal)
 		{
@@ -58,7 +61,7 @@ int binarySearchNode(struct bPlusTreeDataNode *dataRoot,int secVal)
 		}
 	}
 
-	if(*(dataRoot->child + dataRoot->size - 1) < secVal)
+	if(*(dataRoot->val + dataRoot->size - 1) < secVal)
 	{
 		start = dataRoot->size;
 	}
@@ -92,73 +95,135 @@ struct bPlusTreeDataNode* searchInBPlusTreeTree(struct bPlusTreeNode *root,int s
 	return searchData(root->dataRoot,secVal);
 }
 
+struct bPlusTreeDataNode* splitNode(struct bPlusTreeDataNode *dataRoot,struct bPlusTreeDataNode *temp,int nodeSize,int index)
+{
+	struct bPlusTreeDataNode *rightChild = (struct bPlusTreeDataNode*)malloc(sizeof(struct bPlusTreeDataNode)), *parent = (struct bPlusTreeDataNode*)malloc(sizeof(struct bPlusTreeDataNode));
+
+	rightChild->val = (int*)malloc(sizeof(int)*nodeSize);
+	rightChild->child = (struct bPlusTreeDataNode**)malloc(sizeof(struct bPlusTreeDataNode*)*(nodeSize+1));
+	parent->val = (int*)malloc(sizeof(int)*nodeSize);
+	parent->child = (struct bPlusTreeDataNode**)malloc(sizeof(struct bPlusTreeDataNode*)*(nodeSize+1));
+
+	int srcIt = nodeSize,childIt = 0, mid = (nodeSize + 1)/2;
+	boolean isVisited = false;
+
+	printf("%d %d %d\n",nodeSize,mid,index);
+
+	for(childIt = nodeSize - mid; childIt >= 0; --childIt)
+	{
+		printf("1::%d %d %d\n",index,childIt,srcIt);
+		if(index != srcIt || isVisited)
+		{
+			*(rightChild->val + childIt) = *(dataRoot->val + srcIt - 1);
+			if(index != srcIt + 1)
+			{
+				*(rightChild->child + childIt + 1) = *(dataRoot->child + srcIt);
+			}
+			--srcIt;
+		}
+		else
+		{
+			isVisited = true;
+			*(rightChild->val + childIt) = *(temp->val);
+			*(rightChild->child + childIt) = *(temp->child);
+			*(rightChild->child + childIt + 1) = *(temp->child + 1);
+		}
+	}
+	rightChild->size = nodeSize - mid + 1;
+
+	printf("%d %d\n",srcIt,rightChild->size);
+
+	for(childIt = mid - 1; childIt >= 0 && srcIt >= index; --childIt)
+	{
+		if(index == srcIt)
+		{
+			*(dataRoot->val + childIt) = *(temp->val);
+			*(dataRoot->child + childIt) = *(temp->child);
+			*(dataRoot->child + childIt + 1) = *(temp->child + 1);
+		}
+		else
+		{
+			*(dataRoot->val + childIt) = *(dataRoot->val + srcIt - 1);
+			if(index != srcIt + 1)
+			{
+				*(dataRoot->child + childIt + 1) = *(dataRoot->child + srcIt);
+			}
+			--srcIt;
+		}
+	}
+
+	dataRoot->size = mid;
+
+	*(parent->val) = *(rightChild->val);
+	*(parent->child) = dataRoot;
+	*(parent->child + 1) = rightChild;
+	parent->size = 1;
+	dataRoot->parent = rightChild->parent = parent;
+	rightChild->leafNext = dataRoot->leafNext;
+	dataRoot->leafNext = rightChild;
+
+	free(temp);
+
+	return parent;
+}
+
+struct bPlusTreeDataNode* insertIntoTheNode(struct bPlusTreeDataNode *dataRoot,struct bPlusTreeDataNode *temp,int index)
+{
+	int i;
+	for(i = dataRoot->size; i > index; --i)
+	{
+		*(dataRoot->val + i) = *(dataRoot->val + i - 1);
+	}
+	*(dataRoot->val + index) = *(temp->val);
+
+	*(dataRoot->child + index) = *(temp->child);
+	*(dataRoot->child + index + 1) = *(temp->child + 1);
+
+	++(dataRoot->size);
+
+	free(temp);
+
+	return NULL;
+}
+
 struct bPlusTreeDataNode* bTreeInserterroot(struct bPlusTreeDataNode *dataRoot,int nodeSize,int priVal)
 {
 	int index = binarySearchNode(dataRoot,priVal);
+	printf("%d\n",index);
 	if(index >= 0)
 	{
 		return NULL;
 	}
 	else
 	{
+		struct bPlusTreeDataNode *temp;
+		index = -(index + 1);
+		printf("%d %p\n",index,*(dataRoot->child + index));
 		if(*(dataRoot->child + index) != NULL)
 		{
-			struct bPlusTreeDataNode *temp = bTreeInserterroot(*(dataRoot->child + index),nodeSize,priVal);
-			if(temp != NULL)
-			{
-				//check for space and insert value
-			}
-			return temp;
-		}
-		else if(nodeSize == dataRoot->size)
-		{
-			struct bPlusTreeDataNode *rightChild = (struct bPlusTreeDataNode*)malloc(sizeof(struct bPlusTreeDataNode)), *parent = (struct bPlusTreeDataNode*)malloc(sizeof(struct bPlusTreeDataNode));
-
-
-			int srcIt = nodeSize,childIt = 0, mid = (nodeSize + 1)/2;
-			for(childIt = nodeSize - mid; childIt >= 0; --childIt)
-			{
-				if(index != srcIt)
-				{
-					*(rightChild->val + childIt) = *(dataRoot->val + srcIt);
-					--srcIt;
-				}
-				else
-				{
-					*(rightChild->val + childIt) = priVal;
-				}
-			}
-
-			for(childIt = mid; childIt >= 0 && srcIt >= index; --childIt)
-			{
-				if(index == srcIt)
-				{
-					*(dataRoot->val + childIt) = secVal;
-				}
-				else
-				{
-					*(dataRoot->val + childIt) = *(dataRoot->val + srcIt);
-					--srcIt;
-				}
-			}
-
-			*(parent->val) = *(rightChild->val);
-			*(parent->child) = dataRoot;
-			*(parent->child + 1) = rightChild;
-			dataRoot->parent = rightChild->parent = parent;
-			rightChild->leafNext = dataRoot->leafNext;
-			dataRoot->leafNext = rightChild;
-			return parent;
+			temp = bTreeInserterroot(*(dataRoot->child + index),nodeSize,priVal);
 		}
 		else
 		{
-			int i;
-			for(i = dataRoot->size; i > index; --i)
-			{
-				*(dataRoot->val + i) = *(dataRoot->val + i - 1);
-			}
-			*(dataRoot + index) = priVal;
+			temp = (struct bPlusTreeDataNode*)malloc(sizeof(struct bPlusTreeDataNode));
+			temp->val = (int*)malloc(sizeof(int));
+			temp->child = (struct bPlusTreeDataNode**)malloc(sizeof(struct bPlusTreeDataNode*)*2);
+
+			*(temp->val) = priVal;
+			temp->size = 1;
+		}
+		printf("%p\n",temp);
+		if(temp == NULL)
+		{
 			return NULL;
+		}
+		if(nodeSize == dataRoot->size)
+		{
+			return splitNode(dataRoot,temp,nodeSize,index);
+		}
+		else
+		{
+			return insertIntoTheNode(dataRoot,temp,index);
 		}
 	}
 }
