b688d2badabc77395abd5093d18f2a44a21e53c6
sivaranjith <sivaranjith>
2017-12-04 16:34:23 +0530

    day04 updated

--- adventofcode/2017/day04/problem_2.py
+++ adventofcode/2017/day04/problem_2.py
@@ -5,13 +5,9 @@ class Solution042(Solution041):
 	def is_valid_phassparse(self, set_container, token):
 		return_val = True
 		
-		if len(set_container) != 0:
-			for set_element in set_container:
-				if self.is_anagram(set_element, token):
-					return_val = False
-					break
+		for set_element in set_container:
+			if sorted(set_element) == sorted(token):
+				return_val = False
+				break
 		
 		return return_val
-
-	def is_anagram(self, str1, str2):
-		return sorted(str1) == sorted(str2)
