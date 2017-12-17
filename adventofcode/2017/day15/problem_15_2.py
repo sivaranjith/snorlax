f4196182de0784852f8d45f46c57adb58ceb8c0d
sivaranjith <sivaranjith>
2017-12-17 14:46:41 +0530

    aoc day15 code correction

--- adventofcode/2017/day15/problem_15_2.py
+++ adventofcode/2017/day15/problem_15_2.py
@@ -2,13 +2,13 @@ from problem_15_1 import Solution151
 
 class Solution152(Solution151):
 
-	DIVISOR_VALS = {'A':4, 'B':8}
+	DIVISOR_VALS = {'A':3, 'B':7}
 	NO_OF_PAIRS = 5000000
 
 	def process_data(self):
 		return self.generate_pairs(Solution152.NO_OF_PAIRS)
 
 	def compute_next(self, key):
-		while super().compute_next(key) % Solution152.DIVISOR_VALS[key] != 0:
+		while super().compute_next(key) & Solution152.DIVISOR_VALS[key] != 0:
 			pass
 		return self.generator_value[key]
